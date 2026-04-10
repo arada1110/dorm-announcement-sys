3aa42686cf8ad37a04f01a6c0c73f3d8faf23737
Arada Waenwong <waenwong_a@silpakorn.edu>
2026-04-10 19:43:48 +0700

    fix(seed): fix seed file when migrate

--- packages/dal/seeds/announcementCategory.seed.ts
+++ packages/dal/seeds/announcementCategory.seed.ts
@@ -1,32 +1,34 @@
 import { Knex } from "knex";
 
 export async function seed(knex: Knex): Promise<void> {
-    await knex("announcement_categories").delete();
-    await knex("announcement_categories").insert([
-        {
-            category_name: "GENERAL",
-            description: "ข่าวที่ส่งถึงผู้พักทั้งหมด",
-            is_active: true,
-        },
-        {
-            category_name: "ANNOUNCEMENT",
-            description: "ประกาศสำคัญ",
-            is_active: true,
-        },
-        {
-            category_name: "EVENT",
-            description: "กิจกรรม",
-            is_active: true,
-        },
-        {
-            category_name: "PAYMENT",
-            description: "การชำระเงิน",
-            is_active: true,
-        },
-        {
-            category_name: "MAINTENANCE",
-            description: "การซ่อมบำรุง",
-            is_active: true,
-        },
-    ]);
+    await knex("announcement_categories")
+        .insert([
+            {
+                category_name: "GENERAL",
+                description: "ข่าวที่ส่งถึงผู้พักทั้งหมด",
+                is_active: true,
+            },
+            {
+                category_name: "ANNOUNCEMENT",
+                description: "ประกาศสำคัญ",
+                is_active: true,
+            },
+            {
+                category_name: "EVENT",
+                description: "กิจกรรม",
+                is_active: true,
+            },
+            {
+                category_name: "PAYMENT",
+                description: "การชำระเงิน",
+                is_active: true,
+            },
+            {
+                category_name: "MAINTENANCE",
+                description: "การซ่อมบำรุง",
+                is_active: true,
+            },
+        ])
+        .onConflict("category_name")
+        .merge();
 }
